cd5881f7a095535277a88a0c62765c540152ca3b
Jakub Konka <kubkon@jakubkonka.com>
2021-02-20 10:29:54 +0100

    parse archive name and symtab

--- src/Archive.zig
+++ src/Archive.zig
@@ -16,21 +16,24 @@ usingnamespace @import("commands.zig");
 allocator: *Allocator,
 file: fs.File,
 header: ar_hdr,
+name: []u8,
 
 objects: std.ArrayListUnmanaged(Object) = .{},
+
+toc: std.StringArrayHashMapUnmanaged(u64) = .{},
 
 // Archive files start with the ARMAG identifying string.  Then follows a
 // `struct ar_hdr', and as many bytes of member file data as its `ar_size'
 // member indicates, for each member file.
 /// String that begins an archive file.
-pub const ARMAG: *const [SARMAG:0]u8 = "!<arch>\n";
+const ARMAG: *const [SARMAG:0]u8 = "!<arch>\n";
 /// Size of that string.
-pub const SARMAG: u4 = 8;
+const SARMAG: u4 = 8;
 
 /// String in ar_fmag at the end of each header.
-pub const ARFMAG: *const [2:0]u8 = "`\n";
+const ARFMAG: *const [2:0]u8 = "`\n";
 
-pub const ar_hdr = extern struct {
+const ar_hdr = extern struct {
     /// Member file name, sometimes / terminated.
     ar_name: [16]u8,
 
@@ -51,13 +54,51 @@ pub const ar_hdr = extern struct {
 
     /// Always contains ARFMAG.
     ar_fmag: [2]u8,
+
+    const NameOrLength = union(enum) {
+        Name: []const u8,
+        Length: u64,
+    };
+    pub fn nameOrLength(self: ar_hdr) !NameOrLength {
+        const value = getValue(&self.ar_name);
+        const slash_index = mem.indexOf(u8, value, "/") orelse return error.MalformedArchive;
+        const len = value.len;
+        if (slash_index == len - 1) {
+            // Name stored directly
+            return NameOrLength{ .Name = value };
+        } else {
+            // Name follows the header directly and its length is encoded in
+            // the name field.
+            const length = try std.fmt.parseInt(u64, value[slash_index + 1 ..], 10);
+            return NameOrLength{ .Length = length };
+        }
+    }
+
+    pub fn size(self: ar_hdr) !u64 {
+        const value = getValue(&self.ar_size);
+        return std.fmt.parseInt(u64, value, 10);
+    }
+
+    fn getValue(raw: []const u8) []const u8 {
+        return mem.trimRight(u8, raw, &[_]u8{@as(u8, 0x20)});
+    }
 };
 
-pub fn deinit(self: *Archive) void {}
+pub fn deinit(self: *Archive) void {
+    self.allocator.free(self.name);
+    for (self.objects.items) |*object| {
+        object.deinit();
+    }
+    self.objects.deinit(self.allocator);
+    for (self.toc.items()) |entry| {
+        self.allocator.free(entry.key);
+    }
+    self.toc.deinit(self.allocator);
+}
 
 /// Caller owns the returned Archive instance and is responsible for calling
 /// `deinit` to free allocated memory.
-pub fn initFromFile(allocator: *Allocator, name: []const u8, file: fs.File) !Archive {
+pub fn initFromFile(allocator: *Allocator, ar_name: []const u8, file: fs.File) !Archive {
     var reader = file.reader();
     var magic = try readMagic(allocator, reader);
     defer allocator.free(magic);
@@ -73,15 +114,32 @@ pub fn initFromFile(allocator: *Allocator, name: []const u8, file: fs.File) !Arc
     if (!mem.eql(u8, &header.ar_fmag, ARFMAG))
         return error.MalformedArchive;
 
+    // TODO parse ToC of the archive (symbol -> object mapping)
+    const toc_size = try header.size();
+    log.debug("{}", .{toc_size});
+
+    const name_or_length = try header.nameOrLength();
+    var name: []u8 = undefined;
+    switch (name_or_length) {
+        .Name => |n| {
+            name = try allocator.dupe(u8, n);
+        },
+        .Length => |len| {
+            name = try allocator.alloc(u8, len);
+            try reader.readNoEof(name);
+        },
+    }
+    log.debug("{}, {s}", .{ name_or_length, name });
+
     var self = Archive{
         .allocator = allocator,
         .file = file,
         .header = header,
+        .name = name,
     };
+    try self.readSymtab(reader);
+    try self.readStrtab(reader);
 
-    const size = try self.readSize();
-
-    // TODO parse ToC of the archive (symbol -> object mapping)
     // TODO parse objects contained within.
 
     return self;
@@ -98,8 +156,27 @@ fn readMagic(allocator: *Allocator, reader: anytype) ![]u8 {
     return magic.toOwnedSlice();
 }
 
-fn readSize(self: *Archive) !u64 {
-    const slice = mem.trimRight(u8, &self.header.ar_size, &[_]u8{@as(u8, 0x20)});
-    const size = try std.fmt.parseInt(u64, slice, 10);
-    return size;
+fn readSymtab(self: *Archive, reader: anytype) !void {
+    const symtab_size = try reader.readIntLittle(u32);
+    log.debug("{}", .{symtab_size});
+    var buffer = try self.allocator.alloc(u8, symtab_size);
+    defer self.allocator.free(buffer);
+    try reader.readNoEof(buffer);
+
+    var sym_stream = std.io.fixedBufferStream(buffer);
+    var sym_reader = sym_stream.reader();
+
+    while (true) {
+        const n_strx = sym_reader.readIntLittle(u32) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => |e| return e,
+        };
+        const obj = try sym_reader.readIntLittle(u32);
+
+        log.debug("0x{x}, 0x{x}", .{ n_strx, obj });
+    }
+}
+
+fn readStrtab(self: *Archive, reader: anytype) !void {
+    const strtab_size = try reader.readIntLittle(u32);
 }
