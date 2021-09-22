7ea7d772ea4dc9fa350a9da497c356e055947df5
Jakub Konka <kubkon@jakubkonka.com>
2021-09-22 12:09:15 +0200

    elf: parse symbol and string tables

--- src/Elf/Object.zig
+++ src/Elf/Object.zig
@@ -15,17 +15,14 @@ name: []const u8,
 header: ?elf.Elf64_Ehdr = null,
 
 shdrs: std.ArrayListUnmanaged(elf.Elf64_Shdr) = .{},
-shstrtab: std.ArrayListUnmanaged(u8) = .{},
 
 symtab: std.ArrayListUnmanaged(elf.Elf64_Sym) = .{},
 strtab: std.ArrayListUnmanaged(u8) = .{},
 
 symtab_index: ?u16 = null,
-strtab_index: ?u16 = null,
 
 pub fn deinit(self: *Object, allocator: *Allocator) void {
     self.shdrs.deinit(allocator);
-    self.shstrtab.deinit(allocator);
     self.symtab.deinit(allocator);
     self.strtab.deinit(allocator);
     allocator.free(self.name);
@@ -74,6 +71,12 @@ pub fn parse(self: *Object, allocator: *Allocator, target: std.Target) !void {
     self.header = header;
 
     try self.parseShdrs(allocator, reader);
+    try self.parseSymtab(allocator);
+
+    log.debug("symtab:", .{});
+    for (self.symtab.items) |sym, i| {
+        log.debug("  {d}: {s}: {}", .{ i, self.getString(sym.st_name), sym });
+    }
 }
 
 fn parseShdrs(self: *Object, allocator: *Allocator, reader: anytype) !void {
@@ -86,18 +89,67 @@ fn parseShdrs(self: *Object, allocator: *Allocator, reader: anytype) !void {
     var i: u16 = 0;
     while (i < shnum) : (i += 1) {
         const shdr = try reader.readStruct(elf.Elf64_Shdr);
+
+        if (shdr.sh_type == elf.SHT_SYMTAB) {
+            self.symtab_index = i;
+        }
+
         self.shdrs.appendAssumeCapacity(shdr);
     }
 
     // Parse shstrtab
-    const shstrtab_shdr = self.shdrs.items[self.header.?.e_shstrndx];
-    var buffer = try allocator.alloc(u8, shstrtab_shdr.sh_size);
+    var buffer = try self.readShdrContents(allocator, self.header.?.e_shstrndx);
     defer allocator.free(buffer);
+    try self.strtab.appendSlice(allocator, buffer);
+}
 
-    const amt = try self.file.preadAll(buffer, shstrtab_shdr.sh_offset);
+fn parseSymtab(self: *Object, allocator: *Allocator) !void {
+    if (self.symtab_index == null) return;
+
+    const symtab_shdr = self.shdrs.items[self.symtab_index.?];
+
+    // We first read the contents of string table associated with this symbol table
+    // noting the offset at which it is appended to the existing string table, which
+    // we will then use to fixup st_name offset within each symbol.
+    const strtab_offset = @intCast(u32, self.strtab.items.len);
+    var str_buffer = try self.readShdrContents(allocator, @intCast(u16, symtab_shdr.sh_link));
+    defer allocator.free(str_buffer);
+    try self.strtab.appendSlice(allocator, str_buffer);
+
+    var sym_buffer = try self.readShdrContents(allocator, self.symtab_index.?);
+    defer allocator.free(sym_buffer);
+    const syms = @alignCast(@alignOf(elf.Elf64_Sym), mem.bytesAsSlice(elf.Elf64_Sym, sym_buffer));
+    try self.symtab.ensureTotalCapacity(allocator, syms.len);
+
+    for (syms) |sym| {
+        var out_sym = sym;
+        if (sym.st_name > 0) {
+            out_sym.st_name += strtab_offset;
+        } else if (sym.st_info & 0xf == elf.STT_SECTION) {
+            // If the symbol is pointing to a section header, copy the sh_name offset as the new
+            // st_name offset.
+            const shdr = self.shdrs.items[sym.st_shndx];
+            out_sym.st_name = shdr.sh_name;
+        }
+        self.symtab.appendAssumeCapacity(out_sym);
+    }
+}
+
+fn getString(self: Object, off: u32) []const u8 {
+    assert(off < self.strtab.items.len);
+    return mem.spanZ(@ptrCast([*:0]const u8, self.strtab.items.ptr + off));
+}
+
+/// Caller owns the memory.
+fn readShdrContents(self: Object, allocator: *Allocator, shdr_index: u16) ![]const u8 {
+    const shdr = self.shdrs.items[shdr_index];
+    var buffer = try allocator.alloc(u8, shdr.sh_size);
+    errdefer allocator.free(buffer);
+
+    const amt = try self.file.preadAll(buffer, shdr.sh_offset);
     if (amt != buffer.len) {
         return error.InputOutput;
     }
 
-    try self.shstrtab.appendSlice(allocator, buffer);
+    return buffer;
 }
