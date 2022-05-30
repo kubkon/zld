f2893ff6daca7a33001d678a3327c7c26aea5fa4
Jakub Konka <kubkon@jakubkonka.com>
2022-05-30 10:22:54 +0200

    elf: hard-code GOTTPOFF optimisation into TPOFF32 with rewrite

--- src/Elf/Atom.zig
+++ src/Elf/Atom.zig
@@ -341,8 +341,50 @@ pub fn resolveRelocs(self: *Atom, elf_file: *Elf) !void {
                     });
                     mem.writeIntLittle(u64, self.code.items[rel.r_offset..][0..8], tls_offset);
                 } else {
-                    // TODO I believe here we should emit a dynamic relocation.
+                    // TODO I believe here we should emit a dynamic relocation pointing
+                    // at a GOT cell.
                     log.debug("TODO R_X86_64_DTPOFF64: {x}: [0x{x} => 0x{x}] ({s})", .{
+                        rel.r_offset,
+                        source,
+                        tsym.st_value,
+                        tsym_name,
+                    });
+                }
+            },
+            elf.R_X86_64_GOTTPOFF => {
+                const source = @intCast(i64, sym.st_value + rel.r_offset);
+                const is_global_tls = tsym_st_type == elf.STT_TLS and tsym_st_bind == elf.STB_GLOBAL;
+                if (is_global_tls) {
+                    // Since the symbol is defined locally (in the linkage unit), we
+                    // can statically relocate the offset. To put it another way,
+                    // we convert this relocation into TPOFF32.
+                    const global = elf_file.globals.get(tsym_name).?;
+                    const actual_object = elf_file.objects.items[global.file.?];
+                    const actual_tsym = actual_object.symtab.items[global.sym_index];
+                    const base_addr: u64 = base_addr: {
+                        const shdr = if (elf_file.tdata_sect_index) |index|
+                            elf_file.shdrs.items[index]
+                        else
+                            elf_file.shdrs.items[elf_file.tbss_sect_index.?];
+                        break :base_addr shdr.sh_addr;
+                    };
+                    const tls_offset = actual_tsym.st_value - base_addr;
+                    log.debug("R_X86_64_GOTTPOFF: {x}: [0x{x} => 0x{x} (TLS)] ({s})", .{
+                        rel.r_offset,
+                        source,
+                        tls_offset,
+                        tsym_name,
+                    });
+                    // TODO move all this into parsing objects into atoms. By then,
+                    // we should already know if the definition is available or not.
+                    // ADDQ -> LEAQ
+                    log.debug("{x}", .{std.fmt.fmtSliceHexLower(self.code.items[rel.r_offset - 4 ..][0..8])});
+                    self.code.items[rel.r_offset - 2] = 0xc7;
+                    self.code.items[rel.r_offset - 1] = 0xc3;
+                    mem.writeIntLittle(i32, self.code.items[rel.r_offset..][0..4], @intCast(i32, tls_offset));
+                } else {
+                    // TODO
+                    log.debug("TODO R_X86_64_GOTTPOFF: {x}: [0x{x} => 0x{x}] ({s})", .{
                         rel.r_offset,
                         source,
                         tsym.st_value,
@@ -352,8 +394,6 @@ pub fn resolveRelocs(self: *Atom, elf_file: *Elf) !void {
             },
             elf.R_X86_64_TLSGD => {
                 const source = @intCast(i64, sym.st_value + rel.r_offset);
-                // TODO calculate offset into the TLV descriptor.
-                // Ordering assumed is always .tdata followed by .tbss
                 log.debug("TODO R_X86_64_TLSGD: {x}: [0x{x} => 0x{x}] ({s})", .{
                     rel.r_offset,
                     source,
