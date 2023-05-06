a96915f2f43256b58e717e9792e202a37537094d
Jakub Konka <kubkon@jakubkonka.com>
2023-05-06 11:10:51 +0200

    strtab: remove unused API

--- src/strtab.zig
+++ src/strtab.zig
@@ -1,10 +1,3 @@
-const std = @import("std");
-const mem = std.mem;
-
-const Allocator = mem.Allocator;
-const StringIndexAdapter = std.hash_map.StringIndexAdapter;
-const StringIndexContext = std.hash_map.StringIndexContext;
-
 pub fn StringTable(comptime log_scope: @Type(.EnumLiteral)) type {
     return struct {
         const Self = @This();
@@ -12,7 +5,12 @@ pub fn StringTable(comptime log_scope: @Type(.EnumLiteral)) type {
         const log = std.log.scoped(log_scope);
 
         buffer: std.ArrayListUnmanaged(u8) = .{},
-        table: std.HashMapUnmanaged(u32, bool, StringIndexContext, std.hash_map.default_max_load_percentage) = .{},
+        table: std.HashMapUnmanaged(
+            u32,
+            void,
+            StringIndexContext,
+            std.hash_map.default_max_load_percentage,
+        ) = .{},
 
         pub fn deinit(self: *Self, gpa: Allocator) void {
             self.buffer.deinit(gpa);
@@ -25,40 +23,6 @@ pub fn StringTable(comptime log_scope: @Type(.EnumLiteral)) type {
             return result;
         }
 
-        pub const PrunedResult = struct {
-            buffer: []const u8,
-            idx_map: std.AutoHashMap(u32, u32),
-        };
-
-        pub fn toPrunedResult(self: *Self, gpa: Allocator) !PrunedResult {
-            var buffer = std.ArrayList(u8).init(gpa);
-            defer buffer.deinit();
-            try buffer.ensureTotalCapacity(self.buffer.items.len);
-            buffer.appendAssumeCapacity(0);
-
-            var idx_map = std.AutoHashMap(u32, u32).init(gpa);
-            errdefer idx_map.deinit();
-            try idx_map.ensureTotalCapacity(self.table.count());
-
-            var it = self.table.iterator();
-            while (it.next()) |entry| {
-                const off = entry.key_ptr.*;
-                const save = entry.value_ptr.*;
-                if (!save) continue;
-                const new_off = @intCast(u32, buffer.items.len);
-                buffer.appendSliceAssumeCapacity(self.getAssumeExists(off));
-                idx_map.putAssumeCapacityNoClobber(off, new_off);
-            }
-
-            self.buffer.clearRetainingCapacity();
-            self.table.clearRetainingCapacity();
-
-            return PrunedResult{
-                .buffer = buffer.toOwnedSlice(),
-                .idx_map = idx_map,
-            };
-        }
-
         pub fn insert(self: *Self, gpa: Allocator, string: []const u8) !u32 {
             const gop = try self.table.getOrPutContextAdapted(gpa, @as([]const u8, string), StringIndexAdapter{
                 .bytes = &self.buffer,
@@ -67,7 +31,6 @@ pub fn StringTable(comptime log_scope: @Type(.EnumLiteral)) type {
             });
             if (gop.found_existing) {
                 const off = gop.key_ptr.*;
-                gop.value_ptr.* = true;
                 log.debug("reusing string '{s}' at offset 0x{x}", .{ string, off });
                 return off;
             }
@@ -81,17 +44,8 @@ pub fn StringTable(comptime log_scope: @Type(.EnumLiteral)) type {
             self.buffer.appendAssumeCapacity(0);
 
             gop.key_ptr.* = new_off;
-            gop.value_ptr.* = true;
 
             return new_off;
-        }
-
-        pub fn delete(self: *Self, string: []const u8) void {
-            const value_ptr = self.table.getPtrAdapted(@as([]const u8, string), StringIndexAdapter{
-                .bytes = &self.buffer,
-            }) orelse return;
-            value_ptr.* = false;
-            log.debug("marked '{s}' for deletion", .{string});
         }
 
         pub fn getOffset(self: *Self, string: []const u8) ?u32 {
@@ -111,3 +65,10 @@ pub fn StringTable(comptime log_scope: @Type(.EnumLiteral)) type {
         }
     };
 }
+
+const std = @import("std");
+const mem = std.mem;
+
+const Allocator = mem.Allocator;
+const StringIndexAdapter = std.hash_map.StringIndexAdapter;
+const StringIndexContext = std.hash_map.StringIndexContext;
