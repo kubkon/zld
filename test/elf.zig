5461a338384eb1932504d4c19567fce0929c12c1
Jakub Konka <kubkon@jakubkonka.com>
2023-07-07 09:18:52 +0200

    elf: test overriding symbol by exe in a dso

--- test/elf.zig
+++ test/elf.zig
@@ -3,6 +3,7 @@ pub fn addElfTests(b: *Build, opts: Options) *Step {
 
     if (builtin.target.ofmt == .elf) {
         elf_step.dependOn(testDsoIfunc(b, opts));
+        elf_step.dependOn(testDsoPlt(b, opts));
     }
 
     return elf_step;
@@ -40,6 +41,48 @@ fn testDsoIfunc(b: *Build, opts: Options) *Step {
 
     const run = exe.run();
     run.expectStdOutEqual("Hello world\n");
+    test_step.dependOn(&run.step);
+
+    return test_step;
+}
+
+fn testDsoPlt(b: *Build, opts: Options) *Step {
+    const test_step = b.step("test-dso-plt", "");
+
+    const dylib = cc(b, "liba.so", opts);
+    dylib.addArgs(&.{ "-fPIC", "-shared" });
+    dylib.addSourceBytes(
+        \\#include<stdio.h>
+        \\void world() {
+        \\  printf("world\n");
+        \\}
+        \\void real_hello() {
+        \\  printf("Hello ");
+        \\  world();
+        \\}
+        \\void hello() {
+        \\  real_hello();
+        \\}
+    , "a.c");
+    const dylib_out = dylib.saveOutputAs("liba.so");
+
+    const exe = cc(b, null, opts);
+    exe.addSourceBytes(
+        \\#include<stdio.h>
+        \\void world() {
+        \\  printf("WORLD\n");
+        \\}
+        \\void hello();
+        \\int main() {
+        \\  hello();
+        \\}
+    , "main.c");
+    exe.addArg("-la");
+    exe.addPrefixedDirectorySource("-L", dylib_out.dir);
+    exe.addPrefixedDirectorySource("-Wl,-rpath,", dylib_out.dir);
+
+    const run = exe.run();
+    run.expectStdOutEqual("Hello WORLD\n");
     test_step.dependOn(&run.step);
 
     return test_step;
