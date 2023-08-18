be6e334de2a7a449691cedd07cc184743756fdff
Jakub Konka <kubkon@jakubkonka.com>
2023-08-18 18:46:08 +0200

    macho: use reader.readStructBig instead of readFatStruct

--- src/MachO/fat.zig
+++ src/MachO/fat.zig
@@ -8,19 +8,9 @@ const native_endian = builtin.target.cpu.arch.endian();
 
 const MachO = @import("../MachO.zig");
 
-fn readFatStruct(reader: anytype, comptime T: type) !T {
-    // Fat structures (fat_header & fat_arch) are always written and read to/from
-    // disk in big endian order.
-    var res = try reader.readStruct(T);
-    if (native_endian != std.builtin.Endian.Big) {
-        mem.byteSwapAllFields(T, &res);
-    }
-    return res;
-}
-
 pub fn isFatLibrary(file: std.fs.File) bool {
     const reader = file.reader();
-    const hdr = readFatStruct(reader, macho.fat_header) catch return false;
+    const hdr = reader.readStructBig(macho.fat_header) catch return false;
     defer file.seekTo(0) catch {};
     return hdr.magic == macho.FAT_MAGIC;
 }
@@ -31,13 +21,14 @@ pub const Arch = struct {
 };
 
 pub fn parseArchs(file: std.fs.File, buffer: *[2]Arch) ![]const Arch {
-    const fat_header = try readFatStruct(file.reader(), macho.fat_header);
+    const reader = file.reader();
+    const fat_header = try reader.readStructBig(macho.fat_header);
     assert(fat_header.magic == macho.FAT_MAGIC);
 
     var count: usize = 0;
     var fat_arch_index: u32 = 0;
     while (fat_arch_index < fat_header.nfat_arch) : (fat_arch_index += 1) {
-        const fat_arch = try readFatStruct(file.reader(), macho.fat_arch);
+        const fat_arch = try reader.readStructBig(macho.fat_arch);
         // If we come across an architecture that we do not know how to handle, that's
         // fine because we can keep looking for one that might match.
         const arch: std.Target.Cpu.Arch = switch (fat_arch.cputype) {
