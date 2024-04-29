2cfe1a9da4ecd0adda48f98884e56a37898b623f
Jakub Konka <kubkon@jakubkonka.com>
2024-04-29 22:44:18 +0200

    coff: do not set alignment when creating input sections

--- src/Coff/Object.zig
+++ src/Coff/Object.zig
@@ -570,7 +570,8 @@ pub fn initSection(self: Object, atom: *const Atom, coff_file: *Coff) !u16 {
     // TODO take /merge directives into account
     const header = atom.getInputSection(coff_file);
     const full_name = self.getString(header.name);
-    const flags = header.flags;
+    var flags = header.flags;
+    flags.ALIGN = 0;
     const name_sep = mem.indexOfScalar(u8, full_name, '$') orelse full_name.len;
     const name = full_name[0..name_sep];
     return coff_file.getSectionByName(name) orelse try coff_file.addSection(name, flags);
